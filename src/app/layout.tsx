2373540e6a464ff4abd97ba08aba6d490832cf81
swaraj-shubh <shubhh.ab@gmail.com>
2026-04-02 05:20:39 +0530

    gliker font for the heading

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,7 +32,7 @@ export default function RootLayout({
     >
       <body className="flex flex-col min-h-screen">
         <Navbar />
-        
+        <PokeCursor /> {/* 2. Add the component here */}
         <main className="flex-grow">
           {children}
         </main>
